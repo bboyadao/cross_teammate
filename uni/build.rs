d3154219b34acaaabda65b42b68c464290dac7d3
Anh Nguyen <tuananh@chuthe.com>
2026-03-25 15:40:59 +0700

    update build path relative to manifest_dir

--- uni/build.rs
+++ uni/build.rs
@@ -1,13 +1,10 @@
 #![allow(non_snake_case, dead_code)]
-
-// fn main() {
-//     uniffi::generate_scaffolding("teammate.udl").unwrap()
-// }
-
 use camino::Utf8PathBuf;
 
 fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let udl_file = Utf8PathBuf::from(manifest_dir).join("src/teammate.udl");
+    let udl_file: Utf8PathBuf = [manifest_dir.to_string(), "src".to_string(), "teammate.udl".to_string()]
+        .iter()
+        .collect();
     uniffi::generate_scaffolding(&udl_file).unwrap();
 }
